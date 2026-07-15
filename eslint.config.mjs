025502a4ff0778cd9c2574ba2eb85dd1c899c828
Aleksander_De_Deus <aleksandededeus943@gmail.com>
2026-07-15 12:32:24 -0300

    fix: ajuste no documento de configuração

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import nextPlugin from "@next/eslint-plugin-next";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([".next", "dist", "node_modules"]),
+
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx,js,jsx}"],
+
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
     ],
+
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
       globals: globals.browser,
     },
   },
-])
+]);
