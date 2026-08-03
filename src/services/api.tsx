2cdc051765263aff01878d8e6ec0c63349e4dbc7
Aleksander_De_Deus <aleksandededeus943@gmail.com>
2026-08-03 14:42:02 -0300

    fix: ajuste do endpoint da API local

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:'https://financeiro-api-1wmw.onrender.com',
+  baseURL:'http://localhost:8080',
 });
 
 api.interceptors.request.use((config) => {
