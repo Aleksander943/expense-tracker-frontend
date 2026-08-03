7dd5669a8ab5002288b83283ae0ccc427174d015
Aleksander_De_Deus <aleksandededeus943@gmail.com>
2026-08-03 14:34:19 -0300

    feat: integração do providers no layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import './globals.css';
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Controle financeiro",
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Providers>
+        {children}
+        </Providers>
+        </body>
     </html>
   );
 }
