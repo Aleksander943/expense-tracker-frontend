2d13777fb778552217551cd1239f0d2846a0e716
Aleksander_De_Deus <aleksandededeus943@gmail.com>
2026-08-05 14:03:28 -0300

    feat: adiciona middleware de autenticação

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,15 +1,15 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL:'http://localhost:8080',
 });
 
 api.interceptors.request.use((config) => {
+  const token = Cookies.get('token');
   if (typeof window === 'undefined') {
     return config;
   }
-
-  const token = localStorage.getItem('token');
   if (token) {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
