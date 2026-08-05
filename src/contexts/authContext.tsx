b2fda8afccee5809be035c9f5e3f9370acc9da27
Aleksander_De_Deus <aleksandededeus943@gmail.com>
2026-08-05 14:52:02 -0300

    fix: ajuste de auth para pegar o token

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,29 +1,48 @@
-'use client'
-
 import api from "@/services/api";
-import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { createContext, useEffect, useState, type ReactNode } from "react";
 
-interface usuario {
-  id: number;
+interface User {
   name: string;
-  email: string
+  email: string;
 }
 
-export const AuthContext = () => {
-  const [usuario, setUsuario] =  useState< usuario | null >(null);
+interface AuthContextData {
+  user: User
+}
 
-  const informacaoUser = async () => {
-    try {
-      const response = await api.get("/me");
-      setUsuario(response.data)
-      
-    } catch (error) {
-      console.log(error)
-    }
-  };
+interface AuthProviderProps {
+  children: ReactNode;
+}
 
+export const AuthContext = createContext({} as AuthContextData);
+
+export const AuthProvider = ({children}: AuthProviderProps) =>{
+  const [user, setUser] = useState({
+  name: "",
+  email: "",
+});
   useEffect(() => {
-    informacaoUser();
-  },[])
+    const usuario = async() => {
+      const token = Cookies.get('token');
+      if (!token) return;
+      try {
+        const response = await api.get('/me', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-};
+        setUser(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar dados do usuário:', error);
+      }
+    };
+
+    usuario();
+  }, []);
+  
+  return(
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  )
+}
