8c568e1e833af19d9501b0bf445faf2eb8b0a1aa
Aleksander943 <aleksandededeus943@gmail.com>
2026-08-22 21:43:56 -0300

    feat: Criação do array para consulta do mês

---
+++ src/app/relatorio/components/data.ts
@@ -0,0 +1,14 @@
+export const Mes = [
+  { id: 1, nome: "Janeiro", abreviacao: "Jan" },
+  { id: 2, nome: "Fevereiro", abreviacao: "Fev" },
+  { id: 3, nome: "Março", abreviacao: "Mar" },
+  { id: 4, nome: "Abril", abreviacao: "Abr" },
+  { id: 5, nome: "Maio", abreviacao: "Mai" },
+  { id: 6, nome: "Junho", abreviacao: "Jun" },
+  { id: 7, nome: "Julho", abreviacao: "Jul" },
+  { id: 8, nome: "Agosto", abreviacao: "Ago" },
+  { id: 9, nome: "Setembro", abreviacao: "Set" },
+  { id: 10, nome: "Outubro", abreviacao: "Out" },
+  { id: 11, nome: "Novembro", abreviacao: "Nov" },
+  { id: 12, nome: "Dezembro", abreviacao: "Dez" },
+];
